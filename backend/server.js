2c0cf2bc39f65d33cb216cb67a4ab1b467fbcb1c
Pratham <prathamarora9365@gmail.com>
2025-11-23 14:39:37 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ const app = express() ;
 app.use(cookieParser())
 app.use(express.json()) ;
 dotenv.config() ; 
+app.use(express.urlencoded({extended:true})) ;
+app.use(cors({
+    origin: process.env.FRONTEND_URL||"http://localhost:5173",
+    credentials:true,
+}))
 app.use("/auth", authRouter) ;
 
 const promises = [connectDB(),redisConnect()] ;
